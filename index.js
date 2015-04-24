a2a0d8edabab51accd621f50c25fdbd9f132a5c7
steventhomasmiller <steventhomasmiller@gmail.com>
2015-04-23 19:34:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+var asyncSeries = function (list, done)
+{
+  var counter = 0;
+  var callback = function()
+  {
+    counter++;
+    var next = list[counter];
+    next(callback);
+  }
+    var fn = list[counter];
+    fn(callback);
+    //console.log("Hi.");
+};
+
 asyncSeries([
-  function(callback) {
+  function(callback)
+  {
     console.log(1);
     callback();
   }, function(callback) {
@@ -7,7 +22,7 @@ asyncSeries([
       console.log(2);
       callback();
     }, 1000);
-  }, console.log.bind(console, 3);
+  }, console.log.bind(console, 3) // no semi-colon
 ], function(err) {
-  console.log("This should not run unless there are errors.");
+  console.log("This should not run unless there are errors.")
 });
